8f9825c344218172aace5e697f2cdd3b517492b3
Desire4sorrow <56004655+Desire4sorrow@users.noreply.github.com>
2021-03-10 01:30:22 +0300

    Убраны лишние функции.

--- Valuator/Storage/IStorage.cs
+++ Valuator/Storage/IStorage.cs
@@ -5,7 +5,6 @@ namespace Valuator
     public interface IStorage
     {
         void Store(string key, string value);
-        string StoreKey(string key);
         bool TextSignes(string prefix, string text);
         string Load(string key);
     }
