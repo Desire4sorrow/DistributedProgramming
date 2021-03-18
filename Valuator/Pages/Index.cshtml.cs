ff2b81e1b3873d2f52833647f81f5ce521e3dbea
Desire4sorrow <56004655+Desire4sorrow@users.noreply.github.com>
2021-03-18 12:08:54 +0300

    added lab #3

---
+++ Valuator/Pages/Index.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using NATS.Client;
+
+namespace Valuator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IStorage _storage;
+
+        public IndexModel( IStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync(string text)
+        {
+            string id = Guid.NewGuid().ToString();
+
+            string similarityKey = Constants.SimilarityKey + id; //реорганизация принципа подсчета 
+            double similarity = GetSimilarity(text);
+
+            _storage.Store(similarityKey, similarity.ToString()); //преобразуем для корректного отображения
+
+            string textKey = Constants.TextKey + id;
+            _storage.Store(textKey, text);
+            _storage.Load(textKey);
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Task.Factory.StartNew(() => TaskCalculatingRank(id), cts.Token);
+
+            return Redirect($"summary?id={id}");
+        }
+
+        private async Task TaskCalculatingRank(string id)
+        {
+            ConnectionFactory cf = new ConnectionFactory();
+            using (IConnection connection = cf.CreateConnection())
+            {
+                byte[] data = Encoding.UTF8.GetBytes(id);
+                connection.Publish("valuator.processing.rank", data);
+                await Task.Delay(1000);
+
+                connection.Drain();
+                connection.Close();
+            }
+        }
+        double GetSimilarity(string text)
+        {
+            var similarity = _storage.TextSignes("TEXT-", text);
+            if (similarity)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ scripts/start.cmd
@@ -0,0 +1,8 @@
+
+start /d ..\Valuator\ dotnet run --no-build --urls "http://localhost:5001" 
+start /d ..\Valuator\ dotnet run --no-build  --urls "http://localhost:5002"
+
+start /d ..\RankCalculator\ dotnet run --no-build
+start /d ..\RankCalculator\ dotnet run --no-build
+
+start /d..\nginx\ nginx.exe 
